065ba5649a40e07c64378792f5d9356d3fa1faea
kinecon12 <kinecon12@gmail.com>
2023-09-07 16:30:36 -0700

    cor4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
-int main(void){
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+/**
+ * main-Entry Point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+         puts("\"Programming is like building a multilingual puzzle");
+         return (0);
 }
