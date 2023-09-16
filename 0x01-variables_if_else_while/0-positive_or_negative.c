fafec14e488e2915687146d37baf58aa33b86de2
kinecon12 <kinecon12@gmail.com>
2023-09-16 14:54:21 -0700

    0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-#include<stdio.h>
-/* betty style doc for function main goes there */
+#include <stdio.h>
 /**
- * main - Entry point printing andom number
- *Return: Always 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
  */
 int main(void)
 {
@@ -13,13 +13,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if  (n > 0)
-		printf("%d is positive", n);
+	if (n > 0)
+		printf("%d is positive\n", n);
 	else if (n == 0)
-		printf("%d is Zero", n);
+		printf("%d is zero\n", n);
 	else
-		printf("%d is Negative", n);
-	printf("\n");
+		printf("%d is negative\n", n);
 	return (0);
 }
