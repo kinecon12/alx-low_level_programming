005af4f149a87a8ac60092a654b9d278400fb891
kinecon12 <kinecon12@gmail.com>
2023-09-26 02:39:15 -0700

    advance 0x06function project

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -14,5 +14,9 @@ char *string_toupper(char *str);
 char *cap_string(char *str);
 char *leet(char *str);
 char *rot13(char *str);
+char *rot13(char *a);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 
 #endif /* MAIN_H */
