f8c2a71e3d68d77dd27796174be4eacca48f3cb7
kinecon12 <kinecon12@gmail.com>
2023-10-10 18:10:44 -0700

    0x0D-preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file
+ *
+ * Return: Always 0 (Success)
+ * Author: kingsley
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
